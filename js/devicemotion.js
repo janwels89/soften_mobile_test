d6f460f4889119fee980273c3b3a28bb2a0060f7
janwels89 <57909534+janwels89@users.noreply.github.com>
2020-01-27 16:04:05 +0100

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -3,7 +3,7 @@ console.log("I am Alive! - devicemotion.js");
 
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
-    window.addEventListener('devicemotion',function (event) {
+    window.addEventListener('devicemotion', function (event) {
         document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
         document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
         document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
